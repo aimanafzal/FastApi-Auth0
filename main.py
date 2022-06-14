1f2ac39387f3ef2df56c925e7ce3d25107dfeba6
aimanafzal1 <aiman@snowcrash.com>
2022-06-14 19:14:19 +0500

    Response on behalf of verification added

--- main.py
+++ main.py
@@ -22,9 +22,10 @@
 #     return {"item_name": item.name, "item_id": item_id}
 
 from unittest import result
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, Response, status
 from fastapi.security import HTTPBearer
 
+from utils import VerifyToken
 token_auth_scheme = HTTPBearer()
 
 app = FastAPI()
@@ -40,6 +41,10 @@ def public():
     return result
 
 @app.get("/api/private")
-def private (token: str = Depends(token_auth_scheme)):
-    result = token.credentials
+def private (response: Response, token: str = Depends(token_auth_scheme)):
+    result = VerifyToken(token.credentials).verify()
+    if result.get("status"):
+        response.status_code = status.HTTP_400_BAD_REQUEST
+        return result
+    
     return result
